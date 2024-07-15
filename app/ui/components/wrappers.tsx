d1916fce308ea08e5e669fe218f71c181523287e
Mahmoud-Ehab <mo.ehab150@gmail.com>
2024-07-15 11:42:50 +0300

    ItemsWrapper display on mobiles bug fixed.

--- app/ui/components/wrappers.tsx
+++ app/ui/components/wrappers.tsx
@@ -11,12 +11,24 @@ export function DiscountCardsWrapper({cards}: {cards: DiscountInfo[]}) {
 
 export function ItemsWrapper({items}: {items: ItemInfo[]}) {
     return (
-        <div className=" w-auto text-center">
-            <div className="flex flex-col md:flex-row w-full justify-center">
+        <div className="flex flex-row items-center w-auto text-center">
+            <button className="hidden md:inline-block btn btn-circle bg-primary-dark hover:bg-primary-normal text-fill-light border-0">❮</button>
+            <div className="hidden md:flex flex-col md:flex-row w-full md:h-auto justify-center">
                 {items.map((item, i) => 
-                <div key={i} id={"slide"+i} className="my-5 md:my-0">
-                    <ItemCard item={item} />
-                </div>
+                    <div key={i} id={"slide"+i} className="my-5 md:my-0">
+                        <ItemCard item={item} />
+                    </div>
+                )}
+            </div>
+            <button className="hidden md:inline-block btn btn-circle bg-primary-dark hover:bg-primary-normal text-fill-light border-0">❯</button>
+
+            <div className="md:hidden carousel carousel-vertical rounded-box h-96">
+                {items.map((item, i) =>
+                    <div className="carousel-item h-full">
+                        <div key={i} id={"slide"+i}>
+                            <ItemCard item={item} />
+                        </div>
+                    </div>
                 )}
             </div>
         </div>
